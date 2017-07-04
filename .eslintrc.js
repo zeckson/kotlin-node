130d589787c91d4d0da437166878b6c552986884
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2017-07-04 11:57:25 +0300

    add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  ecmaVersion: 6,
+  env: {
+    es6: true,
+    node: true,
+    commonjs: true,
+  },
+  extends: `htmlacademy/es6`,
+  rules: {
+    'no-console': `off`
+  }
+};
