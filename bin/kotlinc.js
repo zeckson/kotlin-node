214c2f82c1b1ed6877aa408fde5f2499dd7453c1
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2017-09-06 03:06:46 +0300

    resolve sources with directory

--- bin/kotlinc.js
+++ bin/kotlinc.js
@@ -3,6 +3,9 @@
 'use strict';
 
 const cli = require(`../lib/cli`);
+const resolve = require(`path`).resolve;
+const existsSync = require(`fs`).existsSync;
+
 const showHelp = () => cli(`-help`).then(console.log).catch(console.error);
 
 const proxyArgs = process.argv.slice(2);
@@ -10,8 +13,19 @@ const proxyArgs = process.argv.slice(2);
 if (proxyArgs.length === 0) {
   showHelp();
 } else {
-  cli(proxyArgs).then(console.log).catch((e) => {
+  const sources = [];
+  for (let i = proxyArgs.length; i--;) {
+    const path = proxyArgs[i];
+    const resolved = resolve(process.cwd(), path);
+    if (existsSync(resolved)) {
+      sources.push(path);
+    } else {
+      break;
+    }
+  }
+  cli(proxyArgs, sources.reverse()).then(console.log).catch((e) => {
     console.error(e);
-    throw new Error(`Compilation failed.`);
+    // eslint-disable-next-line
+    process.exit(1);
   });
 }
